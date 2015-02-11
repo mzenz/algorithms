727fdcfa74983b785fba4ca977e42b5c186738f6
Martin Zenzerovich <martin.zenzerovich@dreamworks.com>
2015-02-11 15:41:56 -0800

    fixed quicksort bug

--- Sorting/Sorting/QuickSort.h
+++ Sorting/Sorting/QuickSort.h
@@ -26,19 +26,45 @@ int partition(std::vector<T>& v, int lo, int hi)
 }
 
 template<class T>
-void sort(std::vector<T>& v, int lo, int hi)
+void sort(std::vector<T>& a, int l, int r)
 {
-	if (hi > lo) {
-		int j = partition(v, lo, hi);
-		sort(v, lo, j-1);
-		sort(v, j+1, hi);
+	int i = l-1, j = r, p = l-1, q = r;
+	auto v = a[r];
+	if (r <= l)
+		return;
+
+	while (true)
+	{
+		while (a[++i] < v);
+
+		while (v < a[--j])
+			if (j == l)
+				break;
+
+		if (i >= j)
+			break;
+		std::swap(a[i], a[j]);
+		if (a[i] == v) { p++; std::swap(a[p], a[i]); }
+		if (v == a[j]) { q--; std::swap(a[j], a[q]); }
 	}
+	std::swap(a[i], a[r]);
+	j = i-1;
+	i = i+1;
+	for (auto k = l; k < p; k++, j--)
+		std::swap(a[k], a[j]);
+	for (auto k = r-1; k > q; k--, i++)
+		std::swap(a[i], a[k]);
+	sort(a, l, j);
+	sort(a, i, r);
 }
 
 // O(N) = N lg N
+// average complexity ~ 1.39*N*lg(N)
+// Faster than mergesort mainly because it doesn't have to copy things to an aux array but instead increments pointers
 template<class T>
 void quickSort(std::vector<T>& v)
 {
+	//
 	shuffle(v);
 	sort(v, 0, (int)v.size() - 1);
 }
--- Sorting/Sorting/main.cpp
+++ Sorting/Sorting/main.cpp
@@ -1,18 +1,11 @@
-#include <iostream>
 #include "SelectionSort.h"
 #include "InsertionSort.h"
 #include "ShellSort.h"
 #include "MergeSort.h"
 #include "QuickSort.h"
-
-template<class T>
-bool isSorted(const std::vector<T>& v) {
-	for (size_t i = 0; i < v.size() - 1; ++i) {
-		if (v[i] > v[i+1])
-			return false;
-	}
-	return true;
-}
+#include "Select.h"
+#include <iostream>
+#include <algorithm>
 
 int main(int argc, const char * argv[]) {
 //	std::vector<double> v { 6.22, 1.23, 4.54, 0.91, 3.14, 0.03, -5.66, -2.01, 10.9, 7.24 };
@@ -24,25 +17,29 @@ int main(int argc, const char * argv[]) {
 	}
 	std::cout << std::endl;
 
+
+//	for (size_t k = 0; k < v.size(); ++k) {
+//		auto v1(v);
+//		std::cout << k << " element = " << (char)select(v1, k) << std::endl;
+//		std::nth_element(v1.begin(), v1.begin() + k, v1.end());
+//		std::cout << k << " element = " << v1[k] << std::endl;
+//	}
+
 //	selectionSort(v);
 //	insertionSort(v);
 //	shellSort(v);
 //	mergeSort(v);
 	quickSort(v);
 
+//	std::sort(v.begin(), v.end());
+//	std::stable_sort(v.begin(), v.end());
+
 	std::cout << "\nafter:\n";
 	for(auto d : v) {
 		std::cout << d;
 	}
 	std::cout << std::endl;
-	std::cout << "\nsorted? " << (isSorted(v) ? "YES" : "NOPE") << std::endl;
-
-//	shuffle(v);
-//	std::cout << "\nshuffled:\n";
-//	for(auto d : v) {
-//		std::cout << d;
-//	}
-//	std::cout << std::endl;
+	std::cout << "\nsorted? " << (std::is_sorted(v.begin(), v.end()) ? "YES" : "NOPE") << std::endl;
 
 	return 0;
 }
